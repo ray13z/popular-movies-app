bb29d73185d08fd66066f567cd71b819a20734fa
Rayner Vaz <rayner92vaz@gmail.com>
2016-08-02 13:27:27 +0530

    Added ButterKnife to DetailActivity

--- app/src/main/java/com/example/rayner/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/rayner/popularmovies/DetailActivity.java
@@ -11,12 +11,23 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DetailActivity extends AppCompatActivity {
+    // Setup Butterknife views
+    @BindView(R.id.detail_title) TextView detail_title;
+    @BindView(R.id.detail_release_date) TextView detail_release_date;
+    @BindView(R.id.detail_rating) TextView detail_rating;
+    @BindView(R.id.detail_overview) TextView detail_overview;
+    @BindView(R.id.detail_image) ImageView detail_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        ButterKnife.bind(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -28,12 +39,12 @@ public class DetailActivity extends AppCompatActivity {
         String release_date = bundle.getString("release_date").split("-")[0]; // Get release year
 
         // Set fields in the Activity
-        ((TextView) findViewById(R.id.detail_title)).setText(original_title);
-        ((TextView) findViewById(R.id.detail_release_date)).setText(release_date);
-        ((TextView) findViewById(R.id.detail_rating)).setText(vote_average);
-        ((TextView) findViewById(R.id.detail_overview)).setText(overview);
+        detail_title.setText(original_title);
+        detail_release_date.setText(release_date);
+        detail_rating.setText(vote_average);
+        detail_overview.setText(overview);
 
-        Picasso.with(this).load(poster_path).into((ImageView) findViewById(R.id.detail_image));
+        Picasso.with(this).load(poster_path).into(detail_image);
 
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
